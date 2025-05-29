fda6967805a8fb30c9d32e4a4e1a3d839929f4bc
Valentina <valentina@cleverplusplus.com>
2025-05-29 22:16:39 +0300

    Initial Commit

---
+++ IOSAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSAssignment
+//
+//  Created by Valentina Ungurean on 29.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOSAssignment/IOSAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAssignmentApp.swift
+//  IOSAssignment
+//
+//  Created by Valentina Ungurean on 29.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct IOSAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
